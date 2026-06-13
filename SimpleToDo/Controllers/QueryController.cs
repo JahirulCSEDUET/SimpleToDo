4370528379cf4ea7d4993f323ff0febed0140f9b
Jahirul Islam <riktonayem@outlook.com>
2026-06-14 02:40:36 +0600

    Mapping using Automapper

--- SimpleToDo/Controllers/QueryController.cs
+++ SimpleToDo/Controllers/QueryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using SimpleToDo.Application.Interfaces;
 using SimpleToDo.Domain.Entities;
 using SimpleToDo.Domain.Enums;
@@ -13,13 +14,15 @@ namespace SimpleToDo.Web.Controllers
         private readonly IFileService _fileService;
         private readonly INotificationService _notificationService;
         private readonly IToDoService _todoService;
-        public QueryController(IQueryService queryService, IUserService userService, IFileService fileService, INotificationService notificationService, IToDoService todoService)
+        private readonly IMapper _mapper;
+        public QueryController(IQueryService queryService, IUserService userService, IFileService fileService, INotificationService notificationService, IToDoService todoService, IMapper mapper)
         {
             _queryService = queryService;
             _userService = userService;
             _fileService = fileService;
             _notificationService = notificationService;
             _todoService = todoService;
+            _mapper = mapper;
         }
 
         [HttpPost]
